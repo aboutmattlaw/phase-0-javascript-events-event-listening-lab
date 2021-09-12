380cd56c6fd2bfad439c1daf17545c63a322d908
Matt <aboutmattlaw@gmail.com>
2021-09-12 09:56:24 -0400

    redo

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
-function addingEventListener() {
-    const input = document.getElementById('input');
 
-    function clickAlert() {
-      alert('I was clicked!');
-    }
-    
-    input.addEventListener('click', clickAlert); 
+function addingEventListener(foo){
+
+const input = document.getElementById('input');
+input.addEventListener('click' , clickAlert)
+
+function clickAlert() {
+  alert("I was clicked!");
 }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function addingEventListener() {
+//     const input = document.getElementById('input');
+
+//     function clickAlert() {
+//       alert('I was clicked!');
+//     }
+    
+//     input.addEventListener('click', clickAlert); 
+// }
